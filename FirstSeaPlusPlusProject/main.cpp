f1d8dc803446ec276208c9ec3bfb17f4dcf97bf9
Jenna Mills <superangelfish555@gmail.com>
2019-01-22 09:11:22 -0700

    Initial commit

---
+++ FirstSeaPlusPlusProject/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  FirstSeaPlusPlusProject
+//
+//  Created by Mills, Jenna on 1/22/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // insert code here...
+    cout << "Let's go on a road trip to flavourtown.\n";
+}
